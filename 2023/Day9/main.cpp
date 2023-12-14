5e26f212b91562611f6179f6b86d75254ab0246e
Cristian <41880357+cgutierrez21@users.noreply.github.com>
2023-12-14 14:02:46 -0600

    Finished Day 9 with help

--- 2023/Day9/main.cpp
+++ 2023/Day9/main.cpp
@@ -1,4 +1,4 @@
-#include <fstream>
+/*#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -7,11 +7,12 @@
 #include <numeric>
 
 int main() {
-    std::ifstream file("data2.txt");
+    std::ifstream file("data.txt");
     std::string line;
     std::vector<std::string> data;
     std::vector<std::vector<int>> sequence;
     std::vector<std::vector<std::vector<int>>> history;
+    std::vector<int> final_nums;
     std::string data_string;
     int total {0};
     int pass_num {1};
@@ -47,6 +48,19 @@ int main() {
         }
         history.push_back(temp);
     }
+    
+    // Print out the history
+    for (auto &element : history) {
+        std::cout << "Pass " << pass_num << std::endl;
+        for (auto &element2 : element) {
+            for (auto &element3 : element2) {
+                std::cout << element3 << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+        pass_num++;
+    }
 
     for (auto &element : history) {
         int increment {element[element.size()-2][0]};
@@ -56,16 +70,32 @@ int main() {
             element[i-1].push_back(new_num);
             increment = new_num;
         }
+        //std::cout << element[0].back() << std::endl;
+        final_nums.push_back(element[0].back());
+        //std::cout << total << std::endl;
+        //std::cout << std::endl;
     }
 
-    for (auto element : history) {
+    // Print out the history
+    pass_num = 1;
+    for (auto &element : history) {
         std::cout << "Pass " << pass_num << std::endl;
-        total += element[0].back();
-        std::cout << element[0].back() << std::endl;
-        std::cout << total << std::endl;
+        for (auto &element2 : element) {
+            for (auto &element3 : element2) {
+                std::cout << element3 << " ";
+            }
+            std::cout << std::endl;
+        }
         std::cout << std::endl;
         pass_num++;
     }
+
+    for (auto &element : final_nums) {
+        std::cout << element << " | ";
+    }
+
+    std::cout << std::endl;
+    total = std::accumulate(final_nums.begin(), final_nums.end(), 0);
 
     std::cout << "Part 1 total: " << total << std::endl;
 
@@ -73,6 +103,64 @@ int main() {
 
 
 
+
+    return 0;
+}*/
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <numeric>
+#include <fstream>
+
+int main() {
+
+    std::string in;
+    std::ifstream file("data.txt");
+    int ans = 0;
+
+    while (getline(file, in, '\n')) {
+
+        std::vector<int> nums;
+        std::stringstream ss(in);
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+
+        // for part2 uncomment this line
+         reverse(nums.begin(), nums.end());
+
+        //int i = nums.size() - 1;
+
+        std::vector<int> sol;
+        sol.push_back(nums.back());
+        while (nums.size() > 0) {
+            for(int i = 0; i < nums.size() - 1; ++i) {
+                nums[i] = nums[i + 1] - nums[i];
+            }
+            nums.resize(nums.size() - 1);
+            sol.push_back(nums.back());
+
+            bool all_zero = true;
+
+            for(auto &n : nums) {
+                if(n != 0) {
+                    all_zero = false;
+                    break;
+                }
+            }
+
+            if(all_zero) break;
+
+        }
+
+        ans += accumulate(sol.begin(), sol.end(), 0);
+
+    }
+
+    std::cout << ans << "\n";
 
     return 0;
 }
